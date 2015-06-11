58f9b8195217d7df8d0911819ed664f0cfbffeb4
Mike.C <mepsmike@gmail.com>
2015-06-11 17:36:42 +0800

    finish index and show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,40 @@
 class GamesController < ApplicationController
-	
+
+	before_action :get_id, :only =>[:show, :destory, :update]
+
+	def index
+		@games=Game.all
+	end
+
+	def show
+		
+	end
+
+	def create
+		
+	end
+
+	def new
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end 
+
+	def edit
+
+	end
+
+	private
+
+	def get_id
+
+		@game= Game.find(params[:id])
+
+	end
 end
