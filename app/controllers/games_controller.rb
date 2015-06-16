9289401bb67b90c493795eb584408c5baaccc158
Mike.C <mepsmike@gmail.com>
2015-06-16 18:08:13 +0800

    revise edit

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,14 @@
 class GamesController < ApplicationController
 
-	before_action :get_id, :only =>[:show, :destroy, :update, :edit]
+	before_action :set_game, :only =>[:show, :destroy, :update]
 
 	def index
-		@games=Game.page(params[:page]).per(5)
-		
-		@newgame=Game.new
-
-
+		prepare_variable_for_index
+		if params[:gid]
+			@game= Game.find(params[:gid])
+		else
+			@game=Game.new
+		end
 	end
 
 	def show
@@ -15,65 +16,57 @@ class GamesController < ApplicationController
 	end
 
 	def create
+		@game=Game.new(get_form)
 
-		@newgame=Game.new(get_form)
-		if @newgame.save
+		if @game.save
 			flash[:notice] = "新增成功"
 			redirect_to games_path
 		else
-			@games=Game.page(params[:page]).per(5)#因為要顯示首頁，所以render前要先抓不然會錯誤
+			prepare_variable_for_index#因為要顯示首頁，所以render前要先抓不然會錯誤
 			render :action => :index
-		
 		end
-		
-		
-		
 	end
 
-	def new
-
-		@game=Game.new
-
-	end
+	#def new
+	#	@game=Game.new
+	#end
 
 	def update
 
-		if@game.update(get_form)
+		if @game.update(get_form)
 			redirect_to games_path
 			flash[:notice] = "更新成功"
 		else
-			@games=Game.page(params[:page]).per(5) #因為要顯示首頁，所以render前要先抓不然會錯誤
+			prepare_variable_for_index#因為要顯示首頁，所以render前要先抓不然會錯誤
 			render :action => :index
 		end
 	end
 
 	def destroy
-
 		Game.destroy(@game)
+
 		redirect_to games_path
 		flash[:notice] = "刪除成功"
 
 	end 
 
-	def edit
-
-		
-		@games=Game.page(params[:page]).per(5)
-		render :action => :index
-
-
-	end
+	#def edit		
+	#	@games=Game.page(params[:page]).per(5)
+	#	render :action => :index
+	#end
 
 	private
 
-	def get_id
-
+	def set_game
 		@game= Game.find(params[:id])
-
 	end
 
 	def get_form
 		params.require(:game).permit(:name, :description, :year)
-
 	end
+
+	def prepare_variable_for_index
+		@games = Game.page(params[:page]).per(5)
+	end
+
 end
